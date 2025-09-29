b706d90b62aa84ffea2e86b18137a5ddeaf0ed88
e3lau <42027797+e8lau@users.noreply.github.com>
2025-09-29 16:45:08 -0700

    Revert "V1.1.2 Astro Bug FIx"
    This reverts commit d9ac167d38e298a206f3565c9e36dd4f6cb52e9a.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
-// If you later reuse this repo for a user root site, set BASE to "/"
-const base = process.env.BASE || '/Flow-Chart/'; // <- match your repo name with trailing slash
-
-export default defineConfig({
-    integrations: [react()],
-    base,
-    // optional but nice for canonical URLs:
-    site: 'https://e8lau.github.io',
-});
+const base = process.env.BASE || '/';
+export default defineConfig({ integrations: [react()], base });
